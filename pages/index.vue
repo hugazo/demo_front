f14d22207b9cfee5a5a6b408bd0058bdb71d3437
Hugo Morales <hugo@42devs.cl>
2021-05-06 20:57:27 -0400

    Update index message

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   .container
-    p Hola soy una web
+    p Hola soy una demo para RIA de conversión de monedas, haz clic en los liks de la barra superior para navegar
 </template>
 
 <script>
