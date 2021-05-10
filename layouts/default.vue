3fac8dbfd52d96eba73489bccda75ed24fb05b92
Hugo Morales <hugo@42devs.cl>
2021-05-09 20:14:14 -0400

    Fix: Mobile menu not showing

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,18 @@
       .navbar-brand
         NuxtLink.navbar-item.is-capitalized(to="/")
           | Demo Web
-      .navbar-menu
+        a.navbar-burger(
+          role="button"
+          aria-label="menu"
+          aria-expanded="false"
+          @click="toggleMenu"
+          )
+          span(aria-hidden="true")
+          span(aria-hidden="true")
+          span(aria-hidden="true")
+      .navbar-menu(
+        :class="{ 'is-active': menuActive }"
+        )
         .navbar-end
           NuxtLink.navbar-item(to="/converter")
             | Converter
@@ -13,3 +24,19 @@
     div
       Nuxt
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      menuActive: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.menuActive = !this.menuActive;
+    },
+  },
+};
+
+</script>
